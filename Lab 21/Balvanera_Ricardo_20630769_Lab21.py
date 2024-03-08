4f6df3b338d90ebf8f44e33e99965d54eea5057a
rgbalvanera <ricardo.balvanera01@utrgv.edu>
2024-03-08 14:28:17 -0600

    3329

--- Lab 21/Balvanera_Ricardo_20630769_Lab21.py
+++ Lab 21/Balvanera_Ricardo_20630769_Lab21.py
@@ -11,7 +11,15 @@ print(db)
 cursor = db.cursor()
 cursor.execute("SELECT * FROM cars;")
 
-result = cursor.fetchall()
+query = "INSERT INTO cars VALUES ( %S , %S , %S , %S , %S , %S , %S )"
 
-for row in result:
-    print(row)
+values = [('3AE9K', 'Honda','Accord',2019,5847,19000,'Silver'),
+          ('GT123','Toyota','Camry',2008,70000,8000,'Black'),
+          ('AB382','Honda','Accord',2014,10000,18000,'White'),
+          ('Y3829','Hyundai','Sonata',2013,20000,17000,'Silver'),
+          ('4TX88','Ford','F150',2017,12,38500,'Red')]
+cursor.executemany(query,values)
+
+db.commit()
+
+print(cursor.rowcount, "record inserted")
